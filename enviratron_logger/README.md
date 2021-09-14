92cfb143c63803625383811409e92339cf7a3f4f
Scott Zarecor <scott@zarecor.com>
2021-09-14 14:41:35 -0500

    adding yaml config handling

---
+++ enviratron_logger/README.md
@@ -0,0 +1,43 @@
+
+# Enviratron Chamber Logger
+
+This is a simple package for logging plant growth chamber data for the Enviratron project. 
+
+The package logs environmental set point and observed data on a per-chamber basis.
+
+Data is logged using the python-json-logger package and each log line is a Python dictionary string.
+
+Here's an example log file entry:
+
+```json
+{
+    "timestamp": "2021-09-14T14:03:48.099574Z", 
+    "level": "INFO", 
+    "co2_actual": 502.0, 
+    "chamber_id": 1, 
+    "co2_target": 500.0, 
+    "humidity_actual": 72.0, 
+    "humidity_target": 74.0, 
+    "humidification_enabled": true, 
+    "dehumidification_enabled": true, 
+    "lighting_1": 100.0, 
+    "lighting_2": 100.0, 
+    "lighting_3": 100.0, 
+    "lighting_4": 100.0, 
+    "lighting_5": 100.0, 
+    "lighting_6": 100.0, 
+    "lighting_7": 100.0, 
+    "temperature_actual": 26.0, 
+    "temperature_target": 26.0, 
+    "air_diverter_state": false, 
+    "watering_actual": 98.0, 
+    "watering_target": 0.0, 
+    "door_state": false, 
+    "curtain_state": false, 
+    "operating_mode": "Manual"
+}
+```
+
+## Example Usage
+
+The logger can be called like so: ```python -m enviratron_logger```
--- enviratron_logger/__main__.py
+++ enviratron_logger/__main__.py
@@ -1,11 +1,15 @@
+import sys
 import logging
-from pythonjsonlogger import jsonlogger
 from datetime import datetime
+from pathlib import Path
+import yaml
+from pythonjsonlogger import jsonlogger
 from .chamber import GrowthChamberControl as gcc
 
 
 class CustomJsonFormatter(jsonlogger.JsonFormatter):
-    """ Custom log format class """
+    """Custom log format class"""
+
     def add_fields(self, log_record, record, message_dict):
         super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
         now = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
@@ -36,14 +40,16 @@ class CustomJsonFormatter(jsonlogger.JsonFormatter):
         return super(CustomJsonFormatter, self).process_log_record(log_record)
 
 
-def get_logger(chamber_id: int):
+def get_logger(chamber_id: int, log_dir_path: Path):
     """Convenience function for getting a logger instance"""
 
     formatter = CustomJsonFormatter("%(timestamp)s %(level)s %(message)s")
     # When working with multiple loggers, it seems that passing a name to getLogger is necessary,
     # or else you'll get things logging to unexpected places
     logger = logging.getLogger(f"chamber_{chamber_id}_logger")
-    log_handler = logging.FileHandler(f"./logs/chamber_{chamber_id}_environment.log")
+    log_handler = logging.FileHandler(
+        log_dir_path / f"chamber_{chamber_id}_environment.log"
+    )
     log_handler.setFormatter(formatter)
     logger.addHandler(log_handler)
     logger.setLevel(logging.INFO)
@@ -51,12 +57,31 @@ def get_logger(chamber_id: int):
 
 
 def main():
-    chamber_ids = (1, 2, 3, 4, 6)
+    DEFAULT_CHAMBER_IDS = (1, 2, 3, 4, 6, 7, 8)
+
+    if len(sys.argv) == 2:
+        yaml_filepath_str = sys.argv[-1]
+
+        with open(yaml_filepath_str, "r") as config_handle:
+            config = yaml.load(config_handle, Loader=yaml.Loader)
+            chamber_ids = config.get("chamber_ids", DEFAULT_CHAMBER_IDS)
+            log_path = Path(config.get("log_directory", "."))
+            log_path.mkdir(exist_ok=True, parents=True)
+
+    else:
+        # No yaml configuration file found in the call
+        chamber_ids = DEFAULT_CHAMBER_IDS
+        log_path = Path(".")
 
     for chamber_id in chamber_ids:
-        logger = get_logger(chamber_id)
+        logger = get_logger(chamber_id, log_dir_path=log_path)
         chamber = gcc(chamber_id)
-        logger.info(msg=chamber.get_state())
+        state = chamber.get_state()
+
+        if state.get("type") == "ConnectionError":
+            logger.error(msg=state)
+        else:
+            logger.info(msg=state)
 
 
 if __name__ == "__main__":
